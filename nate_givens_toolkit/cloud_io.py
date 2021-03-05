6c40179ae2a09bfe5557aa9da9bc729ecc67c743
Nate Givens <nathaniel.givens@gmail.com>
2021-03-05 03:06:50 +0000

    completed implementation of core methods

--- nate_givens_toolkit/cloud_io.py
+++ nate_givens_toolkit/cloud_io.py
@@ -1,10 +1,90 @@
 import os
 import boto3
 import botocore
+import requests
 import pandas as pd
 
-def push_local_data_to_s3(local_filenames, local_dir, s3_path, s3_filenames=None, s3_bucket='nate-givens-ds'):
-    if s3_filenames = None:
-        s3_filenames = local_filenames
-    for file in local_filenames:
-        print(file)
+def push_file_to_s3(local_filename, local_dir, bucket_dir, bucket, bucket_filename=None, overwrite=False):
+    """Copy a local file to an S3 bucket
+    
+    :param local_filename: the name of the local file to be pushed
+    :param local_dir: the local directory where the file to be pushed can be found
+    :param bucket_dir: the bucket directory where the file should be placed
+    :param bucket: the bucket where the file should be placed
+    :param bucket_filename: the name of the file in the bucket (defaults to local_filename)
+    :param overwrite: flag to overwrite the file if it already exists in the bucket (defaults to False)
+    """
+    if bucket_filename is None:
+        bucket_filename = local_filename
+    local_file = os.path.join(local_dir, local_filename)
+    bucket_file = os.path.join(bucket_dir, bucket_filename)
+    if (not file_exists_in_s3(bucket_filename, bucket_dir, bucket)) or overwrite:
+        s3_client = boto3.client('s3')
+        try:
+            response = s3_client.upload_file(local_file, bucket, bucket_file)
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
+        
+
+def pull_data_from_url(url_filename, url, local_dir, local_filename=None, overwrite=False):
+    """Copy a file from URL to local directory
+    
+    :param url_filename: the name of the file at the URL to be pulled
+    :param url: the url where the target file resides
+    :param local_dir: the local directory where the file should be placed
+    :param local_filename: the name of the local copy of the file (defaults to url_filename)
+    :param overwrite: flag to overwrite local file if it exists
+    """
+    if local_filename is None:
+        local_filename = url_filename
+    if (not os.path.isfile(os.path.join(local_dir, local_filename))) or overwrite: 
+        url_file = url + url_filename
+        r = requests.get(url_file, allow_redirects = False)
+        open(os.path.join(local_dir, local_filename), 'wb').write(r.content)
+        
+def pull_file_from_s3(bucket_filename, bucket_dir, bucket, local_dir, local_filename = None, overwrite=False):
+    """Retrieve bucket\bucket_dir\bucket_filename and store as local_dir\local_filename
+    
+    Skips if file does not exist in bucket. If file exists in bucket, only copies locally if file does not exist locally or overwrite is True.
+    
+    :param bucket_filename: the name of the file in the bucket
+    :param bucket_dir: the path to bucket_filename in the bucket
+    :parm local_dir: the path to store the file locally
+    :param local_filename: the name of the file when stored locally (defaults to bucket_filename)
+    :param bucket: the bucket to pull from (defaults to nate-givens-ds)
+    :param overwrite: flag to overwrite a local file (defaults to False)    
+    """
+    if local_filename is None:
+        local_filename = bucket_filename
+    if not file_exists_in_s3(bucket_filename, bucket_dir, bucket):
+        return
+    if (not os.path.isfile(os.path.join(local_dir, local_filename))) or overwrite:
+        source_file = os.path.join(bucket_dir, bucket_filename)
+        dest_file = os.path.join(local_dir, local_filename)
+        s3 = boto3.client('s3')
+        s3.download_file(bucket, source_file, dest_file)
+        return(f'downloading {source_file} from s3 as {dest_file}')
+
+def file_exists_in_s3(bucket_filename, bucket_dir, bucket):
+    """Return True if bucket\bucket_dir\bucket_filename exists, otherwise False
+    
+    :param bucket_filename: the name of the file we're checking the bucket for
+    :parm bucket_dir: the path to bucket_filename in the bucket
+    :param bucket: the bucket to check (defaults to nate-givens-ds)
+    """
+    s3 = boto3.resource('s3')
+    file = os.path.join(bucket_dir, bucket_filename)
+    try:
+        s3.Object(bucket, file).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            return False
+        else:
+            raise
+    else:
+        return True
+    
+def test_me():
+    return 'tested!'
